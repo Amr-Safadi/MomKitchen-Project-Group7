2f371e7beee58ddb512bb636a13440ea592b0123
AmrSafadi <safadiamr02@gmail.com>
2025-03-16 14:55:13 +0200

    fixed the meal images now showing problem

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/MealViewController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/MealViewController.java
@@ -278,6 +278,7 @@ public class MealViewController {
                 checkBoxes[i].setSelected(true); // By default, all preferences are checked
             }
         }
+        updateMealBackground();
     }
 
     @FXML
